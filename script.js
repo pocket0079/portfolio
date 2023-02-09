1e8c17afedcdb5ac71a8c37938cd3b33f35aab86
James <jam3sstotomas@gmail.com>
2023-02-09 00:36:48 -0500

    incorporated changed from portfolio feedback. restructured nav menu logic. added to do app to projects section

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburgerButton = document.querySelector('#hamburger');
-    const menuCloseButton = document.querySelector('#menuClose');
-    const menu = document.querySelector('.slideOutNav');
 
-    hamburgerButton.addEventListener('click', (e) => {
-        menu.classList.add('open');
-    });
+const navLinks = document.querySelectorAll('.mobileNavItem');
+const hamburgerButton = document.querySelector('#hamburger');
+const menuCloseButton = document.querySelector('#menuClose');
+const menu = document.querySelector('.slideOutNav');
 
-    menuCloseButton.addEventListener('click', (e) => {
-        menu.classList.remove('open');
-    });
-})
+const closeMenu = () => {
+    menu.classList.remove('open');
+}
+
+hamburgerButton.addEventListener('click', (e) => {
+    menu.classList.add('open');
+});
+    
+menuCloseButton.addEventListener('click', (e) => {
+    closeMenu();
+});
